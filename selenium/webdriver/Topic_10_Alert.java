8fec6c5a241df354341df23e94a02ec8b900531e
Tu Thuc <Tu Thuc@thuctv>
2022-02-10 15:20:49 +0700

    commit authen alert qqith auto IT

--- selenium/webdriver/Topic_10_Alert.java
+++ selenium/webdriver/Topic_10_Alert.java
@@ -1,11 +1,15 @@
 package webdriver;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -13,9 +17,11 @@ import org.testng.annotations.Test;
 
 public class Topic_10_Alert {
 	WebDriver driver;
+	WebDriverWait explicitWait;
 	Alert alert;
 	String projectPath = System.getProperty("user.dir");
-
+	String authenChrome = projectPath + "\\autoITScript\\authen_chrome.exe";
+	String authenFirefox = projectPath + "\\autoITScript\\authen_firefox.exe";
 	@BeforeClass
 	public void beforeClass() {
 		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
@@ -28,8 +34,10 @@ public class Topic_10_Alert {
 	public void TC_01_Accept_Alert() {
 		driver.get("https://automationfc.github.io/basic-form/index.html");
 		driver.findElement(By.xpath("//button[text()='Click for JS Alert']")).click();
-		// Switch qua alert
-		alert = driver.switchTo().alert();
+		
+		//chờ cho alert xuất hiện + switch qua luôn
+		alert = explicitWait.until(ExpectedConditions.alertIsPresent());
+		
 		Assert.assertEquals(alert.getText(), "I am a JS Alert");
 
 		alert.accept();
@@ -76,7 +84,7 @@ public class Topic_10_Alert {
 		sleepInSecond(3);
 	    Assert.assertTrue(driver.findElement(By.xpath("//p[contains(text(),'Congratulations! You must have the proper credentials.')]")).isDisplayed());
 	}
-	@Test
+	//@Test
 	public void TC_04_Authentication_Alert_II() {
 		String username = "admin";
 		String password = "admin";
@@ -87,7 +95,21 @@ public class Topic_10_Alert {
 		sleepInSecond(3);
 	    Assert.assertTrue(driver.findElement(By.xpath("//p[contains(text(),'Congratulations! You must have the proper credentials.')]")).isDisplayed());
 	}
-	
+	@Test
+	public void TC_04_Authentication_Alert_III() throws IOException {
+		String username = "admin";
+		String password = "admin";
+		driver.get("http://the-internet.herokuapp.com");
+		//Script sẽ chạy trước để chờ authen alert chạy sau
+		if(driver.toString().contains("Firefox")){
+		Runtime.getRuntime().exec(new String[] { authenFirefox,username,password});
+		} else {
+		Runtime.getRuntime().exec(new String[] { authenChrome,username,password});	
+		}
+		driver.findElement(By.xpath("//a[text()='Basic Auth']")).click();
+		sleepInSecond(9);
+	    Assert.assertTrue(driver.findElement(By.xpath("//p[contains(text(),'Congratulations! You must have the proper credentials.')]")).isDisplayed());
+	}
 	public String getAuthenticateLink(String url , String username, String password) {
 		String[] links = url.split("//");
 		url = links[0] + "//" + username + ":" + password + "@" + links[1];
