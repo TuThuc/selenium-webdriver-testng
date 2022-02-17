63635c9e2964784f5ff05689426923d41521982c
Tu Thuc <Tu Thuc@thuctv>
2022-02-17 17:22:04 +0700

    Drop and drag

--- selenium/webdriver/Topic_10_Alert.java
+++ selenium/webdriver/Topic_10_Alert.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
--- selenium/webdriver/Topic_11_Actions_Part_I.java
+++ selenium/webdriver/Topic_11_Actions_Part_I.java
@@ -1,15 +1,27 @@
 package webdriver;
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.Color;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -20,7 +32,7 @@ public class Topic_11_Actions_Part_I {
 	Actions action;
 	JavascriptExecutor jsExecutor;
 	String projectPath = System.getProperty("user.dir");
-
+String dragDropHelperPath = projectPath + "\\dragDropHTML5\\drag_and_drop_helper.js";
 	@BeforeClass
 	public void beforeClass() {
 		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
@@ -84,7 +96,7 @@ public class Topic_11_Actions_Part_I {
 		Assert.assertEquals(retangleNumberSelected.size(), 4);
 		
 	}
-	@Test
+	//@Test
 		public void TC_05_Double_Click() {
 			driver.get("https://automationfc.github.io/basic-form/index.html");
 			//scroll to element
@@ -95,8 +107,8 @@ public class Topic_11_Actions_Part_I {
 			Assert.assertEquals(driver.findElement(By.id("demo")).getText(), "Hello Automation Guys!");
 			
 			}
-	@Test
-	public void TC_07_Right_Click() {
+	//@Test
+	public void TC_06_Right_Click() {
 		driver.get("http://swisnl.github.io/jQuery-contextMenu/demo.html");
 		action.contextClick(driver.findElement(By.xpath("//span[text()='right click me']"))).perform();
 		sleepInSecond(3);
@@ -107,10 +119,128 @@ public class Topic_11_Actions_Part_I {
 	sleepInSecond(3);
 	Assert.assertEquals(driver.switchTo().alert().getText(), "clicked: paste");
 	driver.switchTo().alert().accept();
+	sleepInSecond(4);
 	Assert.assertFalse(driver.findElement(By.cssSelector("li.context-menu-icon-paste")).isDisplayed());
 
 	}
-	@AfterClass
+	//@Test
+	public void TC_07_Drag_Drop_Html4() {
+		driver.get("https://automationfc.github.io/kendo-drag-drop/");
+		WebElement smallCirle = driver.findElement(By.id("draggable"));
+		WebElement bigCirle = driver.findElement(By.id("droptarget"));
+		
+		action.dragAndDrop(smallCirle, bigCirle).perform();
+		sleepInSecond(3);
+		Assert.assertEquals(bigCirle.getText(), "You did great!");
+		String rgbaColor = bigCirle.getCssValue("background-color");
+		System.out.println("RGB:"+rgbaColor);
+		String hexaColor = Color.fromString(rgbaColor).asHex().toLowerCase();
+		Assert.assertEquals(hexaColor, "#03a9f4");
+		
+	}
+	//@Test
+		public void TC_08_Drag_Drop_Html5ByCss() throws IOException {
+			driver.get("https://automationfc.github.io/drag-drop-html5/");
+			String collumnACss ="#column-a";
+			String collumnBCss ="#column-b";
+			
+			String dragDropHelperContent = getContentFile(dragDropHelperPath);
+			
+			dragDropHelperContent = dragDropHelperContent + "$(\"" + collumnACss + "\").simulateDragDrop({ dropTarget: \"" + collumnBCss + "\"});";
+			//Drag and drop A to B
+			jsExecutor.executeScript(dragDropHelperContent); 
+			sleepInSecond(3);
+			Assert.assertEquals(driver.findElement(By.cssSelector("#column-a>header")).getText(), "B");
+			Assert.assertEquals(driver.findElement(By.cssSelector("#column-b>header")).getText(), "A");
+			
+			//Drag and drop B to A
+			jsExecutor.executeScript(dragDropHelperContent); 
+			sleepInSecond(3);
+			Assert.assertEquals(driver.findElement(By.cssSelector("#column-a>header")).getText(), "A");
+			Assert.assertEquals(driver.findElement(By.cssSelector("#column-b>header")).getText(), "B");
+		
+			
+			
+		}
+	@Test
+	public void TC_09_Drag_Drop_Html5ByXpath() throws AWTException {
+		driver.get("https://automationfc.github.io/drag-drop-html5/");
+		String collumnAXpath ="//div[@id='column-a']";
+		String collumnBXpath ="//div[@id='column-b']";
+		
+		//Drag A to B
+		dragAndDropHTML5ByXpath(collumnAXpath, collumnBXpath);
+		sleepInSecond(2);
+		Assert.assertEquals(driver.findElement(By.cssSelector("#column-a>header")).getText(), "B");
+		Assert.assertEquals(driver.findElement(By.cssSelector("#column-b>header")).getText(), "A");
+		//Drag B to A
+				dragAndDropHTML5ByXpath(collumnAXpath, collumnBXpath);
+				sleepInSecond(2);
+				Assert.assertEquals(driver.findElement(By.cssSelector("#column-a>header")).getText(), "A");
+				Assert.assertEquals(driver.findElement(By.cssSelector("#column-b>header")).getText(), "B");
+	}
+		public String getContentFile(String filePath) throws IOException {
+			Charset cs = Charset.forName("UTF-8");
+			FileInputStream stream = new FileInputStream(filePath);
+			try {
+				Reader reader = new BufferedReader(new InputStreamReader(stream, cs));
+				StringBuilder builder = new StringBuilder();
+				char[] buffer = new char[8192];
+				int read;
+				while ((read = reader.read(buffer, 0, buffer.length)) > 0) {
+					builder.append(buffer, 0, read);
+				}
+				return builder.toString();
+			} finally {
+				stream.close();
+			}
+		}
+		public void dragAndDropHTML5ByXpath(String sourceLocator, String targetLocator) throws AWTException {
+
+			WebElement source = driver.findElement(By.xpath(sourceLocator));
+			WebElement target = driver.findElement(By.xpath(targetLocator));
+
+			// Setup robot
+			Robot robot = new Robot();
+			robot.setAutoDelay(500);
+
+			// Get size of elements
+			Dimension sourceSize = source.getSize();
+			Dimension targetSize = target.getSize();
+
+			// Get center distance
+			int xCentreSource = sourceSize.width / 2;
+			int yCentreSource = sourceSize.height / 2;
+			int xCentreTarget = targetSize.width / 2;
+			int yCentreTarget = targetSize.height / 2;
+
+			Point sourceLocation = source.getLocation();
+			Point targetLocation = target.getLocation();
+			System.out.println(sourceLocation.toString());
+			System.out.println(targetLocation.toString());
+
+			// Make Mouse coordinate center of element
+			sourceLocation.x += 20 + xCentreSource;
+			sourceLocation.y += 110 + yCentreSource;
+			targetLocation.x += 20 + xCentreTarget;
+			targetLocation.y += 110 + yCentreTarget;
+
+			// Move mouse to drag from location
+			robot.mouseMove(sourceLocation.x, sourceLocation.y);
+
+			// Click and drag
+			robot.mousePress(InputEvent.BUTTON1_MASK);
+			robot.mousePress(InputEvent.BUTTON1_MASK);
+			robot.mouseMove(((sourceLocation.x - targetLocation.x) / 2) + targetLocation.x, ((sourceLocation.y - targetLocation.y) / 2) + targetLocation.y);
+
+			// Move to final position
+			robot.mouseMove(targetLocation.x, targetLocation.y);
+
+			// Drop
+			robot.mouseRelease(InputEvent.BUTTON1_MASK);
+		}
+
+		@AfterClass
 	public void afterClass() {
 		driver.quit();
 	}
