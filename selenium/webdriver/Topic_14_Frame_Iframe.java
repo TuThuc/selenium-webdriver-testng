b0d93eb6e11923bf132ebc387f75f0b87bd79cb4
Tu Thuc <Admin@DESKTOP-2RT7I3F>
2022-02-23 22:56:25 +0700

    iframe 2

--- selenium/webdriver/Topic_14_Frame_Iframe.java
+++ selenium/webdriver/Topic_14_Frame_Iframe.java
@@ -2,8 +2,10 @@ package webdriver;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -24,6 +26,24 @@ public class Topic_14_Frame_Iframe {
 	public void TC_01_Iframe() {
 	driver.get("https://kyna.vn/");	
 	
+	// Switch vào iframe facebook
+	driver.switchTo().frame(driver.findElement(By.xpath("//div[@class='face-content']/iframe")));
+	
+	String kynaFacebookLikes = driver.findElement(By.xpath("//a[@title='Kyna.vn']/parent::div/following-sibling::div")).getText();
+	Assert.assertEquals(kynaFacebookLikes, "167K likes");
+	//Switch về trang parent
+	driver.switchTo().defaultContent();
+	//Switch vao iframe feedback
+	driver.switchTo().frame("_hjRemoteVarsFrame");
+	driver.findElement(By.xpath("//div[@class='_hj_feedback_container']//div[text()='Phản hồi']")).click();
+	sleepInSecond(2);
+	driver.findElement(By.xpath("//div[text()='Rất thích']")).click();
+	//Swich ve trang parent
+	driver.switchTo().defaultContent();
+	driver.findElement(By.id("live-search-bar")).sendKeys("Excel");
+	driver.findElement(By.cssSelector("button.search-button")).click();
+
+
 	}
 
 	@Test
