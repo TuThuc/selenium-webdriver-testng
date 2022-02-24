fb88d06020a9d66abc3c06bc3200def0b22e224a
Tu Thuc <Tu Thuc@thuctv>
2022-02-24 16:51:26 +0700

    frame

--- selenium/webdriver/Topic_14_Frame_Iframe.java
+++ selenium/webdriver/Topic_14_Frame_Iframe.java
@@ -1,10 +1,13 @@
 package webdriver;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -22,8 +25,9 @@ public class Topic_14_Frame_Iframe {
 	
 	}
 
-	@Test
+	//@Test
 	public void TC_01_Iframe() {
+		
 	driver.get("https://kyna.vn/");	
 	
 	// Switch vào iframe facebook
@@ -34,21 +38,37 @@ public class Topic_14_Frame_Iframe {
 	//Switch về trang parent
 	driver.switchTo().defaultContent();
 	//Switch vao iframe feedback
-	driver.switchTo().frame("_hjRemoteVarsFrame");
-	driver.findElement(By.xpath("//div[@class='_hj_feedback_container']//div[text()='Phản hồi']")).click();
+	driver.switchTo().frame("cs_chat_iframe");
+	driver.findElement(By.cssSelector("div.border_overlay")).click();
 	sleepInSecond(2);
-	driver.findElement(By.xpath("//div[text()='Rất thích']")).click();
+	driver.findElement(By.cssSelector("input.input_name")).sendKeys("Test");
+	driver.findElement(By.cssSelector("input.input_phone")).sendKeys("09873132153");
+	new Select(driver.findElement(By.id("serviceSelect"))).selectByVisibleText("TƯ VẤN TUYỂN SINH");
+	sleepInSecond(3);
 	//Swich ve trang parent
 	driver.switchTo().defaultContent();
-	driver.findElement(By.id("live-search-bar")).sendKeys("Excel");
+	String keyword = "Excel";
+	driver.findElement(By.id("live-search-bar")).sendKeys(keyword);
 	driver.findElement(By.cssSelector("button.search-button")).click();
-
+	sleepInSecond(3);
+	List<WebElement> courseNames = driver.findElements(By.cssSelector("div.content>h4"));
+    for (WebElement course : courseNames) {
+		System.out.println(course.getText().toLowerCase());
+		Assert.assertTrue(course.getText().toLowerCase().contains(keyword.toLowerCase()));
+	}
 
 	}
 
 	@Test
 	public void TC_02_Frame() {
-		
+		driver.get("https://netbanking.hdfcbank.com/netbanking/");
+		// Switch to frame
+		driver.switchTo().frame("login_page");
+		driver.findElement(By.name("fldLoginUserId")).sendKeys("Automationtest");
+		driver.findElement(By.cssSelector("a.login-btn")).click();
+		sleepInSecond(3);
+		//Verify pass textbox is display
+		Assert.assertTrue(driver.findElement(By.name("fldPassword")).isDisplayed());
 	}
 
 	@AfterClass
